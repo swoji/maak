989c30df64bf77341a5b89aaa80bcbdd19e2c43e
swoji <davidookuku@gmail.com>
2025-08-14 13:14:28 +0300

    updated favico ico

---
+++ config/site.ts
@@ -0,0 +1,6 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "MAAK | Finance app",
+  description: "This is a finance management app with csv import support"
+}
